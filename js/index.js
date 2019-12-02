94f9c0540f27d98086aa3733fb54f34371bcdea3
Tristan Depew <tristan.depew@gmail.com>
2019-12-02 15:08:44 -0500

    added nav

--- js/index.js
+++ js/index.js
@@ -40,3 +40,11 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+//Nav
+
+const nav = document.querySelector("nav a")
+nav.forEach( element => {
+  element.textContent = siteContent["nav.length"]
+})
